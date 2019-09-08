200c03f1601588dbad534b9714bc746cba258639
Den <den@mail.ru>
2019-09-08 13:30:07 +0300

    first commit

---
+++ resources/nuxt/pages/me.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>Hello {{ user.name }}!</h1>
+		<nuxt-link to="/">Home page</nuxt-link>
+		<nuxt-link to="/me">Me</nuxt-link>
+	</div>
+</template>
+
+<script>
+    export default {
+        // https://github.com/nuxt-community/axios-module
+        async asyncData({ app }) {
+            const user = await app.$axios.$get('/me');
+            return { user };
+        },
+    };
+</script>
